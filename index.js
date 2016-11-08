a9bce1fa4c3c5a520c539b9a3a01884052bf3618
krissasantos <krissa.santos31@gmail.com>
2016-11-08 05:12:09 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,37 @@ const episodes = [
 ];
 
 const finaleEpisode = { id: 's06e10', title: 'The Winds of Winter' };
+
+function addToPlaylist(playlist, episode) {
+  // return playlist.append(episode)
+  return [...playlist, episode]
+}
+
+
+function removeFromPlaylist(playlist, episode) {
+  return playlist.slice(1)
+}
+
+function getNextEpisodeInPlaylist(playlist) {
+  var playlist_arr = playlist.slice()
+  return playlist[0]
+}
+
+
+
+function bingeWatch(playlist){
+  if (playlist.length !== 0) {
+     var next = getNextEpisodeInPlaylist(playlist)
+      //gets you the first episode of every update array
+      bingeWatch(removeFromPlaylist(playlist, next))
+      // give you updated array
+      return "Please let there be more!"
+
+  }
+     
+}
+
+
+
+
+
